caeac45baf8c0582d9c1f6772237c4a9a32f2c6c
unknown <pragnavalka@gmail.com>
2020-11-15 22:41:37 +0530

    deepavali

---
+++ vib.js
@@ -0,0 +1,20 @@
+var pics = [
+    "Images/AR1.gif", 		//0
+    "Images/AR2.gif", 		//0
+    "Images/AR3.gif", 		//0
+    "Images/AR4.gif", 		//0
+    "Images/AR5.gif", 		//0
+    
+]
+
+var btn = document.querySelector("button")
+var img = document.querySelector("img")
+var counter = 1
+
+btn.addEventListener("click", function(){
+	if(counter === 5){
+		counter = 0
+	}
+	img.src = pics[counter]
+	counter = counter + 1
+});
